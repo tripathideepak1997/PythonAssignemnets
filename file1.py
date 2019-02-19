d385327be2285d15860797b406af8788645c7179
Deepak Tripathi <deepak.tripathi@tothenew.com>
2019-02-19 15:56:45 +0530

    Added the 6th 7th and 8th questions also

---
+++ file1.py
@@ -0,0 +1,17 @@
+
+# 6. Reassign 'hello' in this nested list to say 'goodbye' instead:
+
+list3 = [1,2,[3,4,'hello']]
+list3[2][2]= 'goodbye'
+print(list3)
+
+
+# 7. From the given list list5 create a set
+#     list5 = [1,2,2,33,4,4,11,22,3,3,2]
+list5 = [1,2,2,33,4,4,11,22,3,3,2]
+s = set(list5)
+print(s)
+
+# 8, In string information count the total number of i.
+str = 'helloimintheroomintheoffice'
+print(str.count('i'))
